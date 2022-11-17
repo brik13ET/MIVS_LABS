5b0787d6d3feec87ab3191673d23461921f18626
brik13ET <aleev.ibragim0909@gmail.com>
2022-11-18 01:26:38 +0400

    cleanup

---
+++ LAB1/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace MIVS_LAB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Расчёт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(173, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Задание";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // chart2
+            // 
+            this.chart2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea3.AxisX.IsLabelAutoFit = false;
+            chartArea3.AxisX.LabelStyle.Angle = 45;
+            chartArea3.AxisX.LabelStyle.Format = "{:0.00}";
+            chartArea3.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea3);
+            this.chart2.Location = new System.Drawing.Point(3, 3);
+            this.chart2.Name = "chart2";
+            this.chart2.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Excel;
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.Name = "Series1";
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series5.Name = "Series2";
+            this.chart2.Series.Add(series4);
+            this.chart2.Series.Add(series5);
+            this.chart2.Size = new System.Drawing.Size(591, 206);
+            this.chart2.TabIndex = 4;
+            this.chart2.Text = "chart2";
+            this.chart2.Click += new System.EventHandler(this.chart2_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(191, 12);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.chart1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.chart2);
+            this.splitContainer1.Size = new System.Drawing.Size(597, 426);
+            this.splitContainer1.SplitterDistance = 213;
+            this.splitContainer1.TabIndex = 5;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(12, 107);
+            this.maskedTextBox1.Mask = "000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(173, 20);
+            this.maskedTextBox1.TabIndex = 6;
+            this.maskedTextBox1.Text = "15";
+            this.maskedTextBox1.ValidatingType = typeof(int);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "m:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 232);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 8;
+            // 
+            // chart1
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea4);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Name = "chart1";
+            series6.ChartArea = "ChartArea1";
+            series6.Name = "Series1";
+            this.chart1.Series.Add(series6);
+            this.chart1.Size = new System.Drawing.Size(597, 213);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ LAB1/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MIVS_LAB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            f = new TaskForm();
+            f.Hide();
+        }
+
+        TaskForm f;
+        Random rgen = new Random((int)DateTime.Now.Second);
+
+        private double AvgDist(double a, double b)
+        {
+            return a + rgen.NextDouble() * (b - a);
+        }
+
+        private double NormDist(float micro, float D, float X)
+        {
+            double S = 0;
+            double w = Math.Sqrt(D);
+            for (int i = 0; i < m * m; i++)
+            {
+                S += X;
+            }
+            return (S - micro * m * m) / Math.Sqrt(m * m * D);
+        }
+
+        private double NormDist(float micro, float D)
+        {
+            double S = 0;
+            double w = Math.Sqrt(D);
+            for (int i = 0; i < m*m; i++)
+            {
+                S += AvgDist(micro -w, micro + w);
+            }
+            return (S - micro * m*m) / Math.Sqrt(m*m*D);
+        }
+
+        private double StudentDistribution(int m)
+        {
+            float[] x = new float[m];
+            for (int i = 0; i < m; i++)
+            {
+                x[i] = (float)NormDist(0, 1);
+            }
+            double sq_sum = 0;
+            for (int i = 1; i < m; i++)
+            {
+                sq_sum = x[i] * x[i];
+            }
+            return x[0] * Math.Sqrt(m) / (Math.Sqrt(sq_sum / m));
+        }
+
+        private float[] grid(float from, float to, int count)
+        {
+            float[] ret = new float[count];
+            h = Math.Abs(to - from) / count;
+            ret[0] = from;
+            for (int i = 1; i < count; i++)
+            {
+                ret[i] = ret[i - 1] + (float)h;
+            }
+            return ret;
+        }
+        double h = 0;
+        const int n = 15;
+        const float alpha = 0.05f;
+        const float y0 = 23.7f;
+        int N = 1000;
+        int m = 2;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int.TryParse(maskedTextBox1.Text, out m);
+            float[] a = new float[N];
+            var cnt = new int[n];
+            for (int i = 0; i < N; i++)
+            {
+                a[i] = (float)StudentDistribution(m);
+                //a[i] = (float)NormDist(0, 1);
+            }
+            float max_A = a[0];
+            float min_A = a[0];
+            for (int i = 0; i < N; i++)
+            {
+                if (a[i] > max_A)
+                    max_A = a[i];
+                if (a[i] < min_A)
+                    min_A = a[i];
+            }
+            var gr = grid(min_A, max_A, n);
+            System.Array.Sort(a);
+            for (int i = 0; i < N; i++)
+            {
+                if (a[i] < gr[0])
+                    cnt[0]++;
+                else if (a[i] >= gr[n - 1])
+                    cnt[n - 1]++;
+                else
+                {
+                    int gr_i = 1;
+                    while (a[i] >= gr[gr_i] || a[i] < gr[gr_i - 1])
+                        gr_i++;
+                    cnt[gr_i]++;
+                }
+            }
+
+            chart1.Series[0].Points.Clear();
+            chart2.Series[0].Points.Clear();
+            for (int i = 0; i < n; i++)
+            {
+                chart1.Series[0].Points.AddXY(gr[i], 1f*cnt[i] / N );
+            }
+            for (int i = 0; i < N; i++)
+            {
+                chart2.Series[0].Points.AddXY(a[i], i * 1f / N);
+                
+            }
+
+            double hi2_sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                hi2_sum += (cnt[i]* cnt[i]) * n / N;
+            }
+            double hi2 = n * hi2_sum / N;
+            if (hi2 < y0)
+                label2.Text = ("Гипотеза принята (alpha = 0.05, [n-1] = 14, hi^2 = " + hi2 + ").");
+            else
+                label2.Text = ("hi ^ 2 = " + hi2 + ").");
+
+
+            chart1.Invalidate();
+            chart2.Invalidate();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            f.Show();
+        }
+
+        private void chart2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
